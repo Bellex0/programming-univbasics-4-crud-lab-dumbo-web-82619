0368255ea34b9e7ad588c023b1258b7f2a4ef070
Belle P <bellepx3@gmail.com>
2019-07-11 04:00:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,19 +7,19 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  animals << "chinchilla"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  animals.unshift("kangaroo")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  animals.pop()
 end
 
 def remove_element_from_start_of_array(array)
-  
+  animals.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
